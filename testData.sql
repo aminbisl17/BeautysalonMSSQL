15808aeb122263912e6b541fc79455045f1b37da
amin bislimaj <amin.b05@icloud.com>
2026-01-16 12:19:47 +0100

    v0.5.5

---
+++ testData.sql
@@ -0,0 +1,17 @@
+
+
+
+/*
+declare @array historiku_detajet_array
+
+insert into @array(emri_sherbimit, emri_atributit, pershkrimi, pagesa)
+values(null, null, null, null),
+(null, null, null, null),
+(null, null, null, null);
+
+EXECUTE register_history @ID = 8, @id_employee = 1, @historiku_detajet = @array;
+ */
+--select h.*, d.* from historiku h join historiku_detajet d on h.id_historikut = h.id_historikut;
+--select * from historiku;
+
+--delete from historiku;
